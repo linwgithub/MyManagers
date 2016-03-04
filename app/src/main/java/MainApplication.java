cd9d22f1d35ceaa8695124273fd7dec5a0039524
myvinnie <770601773@qq.com>
2016-03-04 11:21:36 +0800

    Add Leancould

---
+++ app/src/main/java/MainApplication.java
@@ -0,0 +1,12 @@
+import android.app.Application;
+
+/**
+ * Created by linw on 16/3/4.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
